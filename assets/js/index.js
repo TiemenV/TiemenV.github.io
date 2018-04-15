8b78aa9e3d2e911e02c21377ae2fd2c497f4d887
Tiemen Vermote <tiemen.vermote@gmail.com>
2018-04-15 20:37:51 +0200

    cleanup of leftover debugging functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,12 +3,11 @@
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
-    console.log("init");
+    
     document.querySelector("button").addEventListener("click", fetchJoke);
 }
 
 function fetchJoke() {
-    console.log("fetchJoke");
 
     fetch('https://api.chucknorris.io/jokes/random')
         .then(function(response) {
